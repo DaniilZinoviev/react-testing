2e28c69d88d801e747eb7d7f207944ed207931d4
dzinoviev <danzino21@gmail.com>
2020-12-19 21:59:10 +0200

    Testing with fireEvent

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("App component", () => {
@@ -6,5 +6,29 @@ describe("App component", () => {
     const { getByText } = render(<App />);
     const element = getByText(/learn react/i);
     expect(element).toBeInTheDocument();
+  });
+});
+
+describe("events", () => {
+  it("checkbox", () => {
+    const handleOnChange = jest.fn();
+    const { container } = render(
+      <input type="checkbox" onChange={handleOnChange} />
+    );
+    const checkbox = container.firstChild;
+    expect(checkbox).not.toBeChecked();
+    fireEvent.click(checkbox);
+    expect(handleOnChange).toHaveBeenCalled();
+    expect(checkbox).toBeChecked();
+  });
+
+  it("input focus", () => {
+    const { getByTestId } = render(
+      <input type="text" data-testid="simple-input" />
+    );
+    const input = getByTestId("simple-input");
+    expect(input).not.toHaveFocus();
+    input.focus();
+    expect(input).toHaveFocus();
   });
 });
--- src/components/Search/Search.spec.js
+++ src/components/Search/Search.spec.js
@@ -1,18 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Search from "./Search";
 
 describe("Search component", () => {
-  // it("Should render text 'Search'", () => {
-  //   render(<Search />);
-  //   const text = /Search:/i;
-  //   expect(screen.getByText(text)).toBeInTheDocument();
-  //   expect(screen.getByRole("textbox")).toBeInTheDocument();
-  //   expect(screen.getByLabelText("Search:")).toBeInTheDocument();
-  //   expect(screen.getByPlaceholderText("Search...")).toBeInTheDocument();
-  //   expect(screen.getByAltText("Search image")).toBeInTheDocument();
-  //   expect(screen.getByDisplayValue("")).toBeInTheDocument();
-  // });
-
   it("image should be fluid", async () => {
     render(<Search />);
     expect(screen.getByAltText("Search image")).toHaveClass("img-fluid");
@@ -28,6 +17,16 @@ describe("Search component", () => {
     render(<Search />);
 
     expect(screen.getByAltText(/search/i)).toBeEmptyDOMElement();
-    expect(screen.getByAltText(/search/i)).toHaveAttribute('id');
+  });
+
+  it('(fireEvent) should updates after input change', () => {
+    render(<Search />);
+    const input = screen.getByLabelText(/search/i);
+    expect(input).toHaveValue('');
+    fireEvent.change(input, {
+      target: { value: 'testing' }
+    });
+    expect(input).toHaveValue('testing');
   })
+
 });
